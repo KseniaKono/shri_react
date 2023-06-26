de4fd7a2ab5f3888f0978f179b35d2771fe1a9d3
KseniaKono <100347521+KseniaKono@users.noreply.github.com>
2023-06-27 02:18:41 +0400

    i i

--- my-app/components/redux/tickets/selector.js
+++
@@ -1,7 +0,0 @@
-const selectTickets = (state) => state.tickets;
-
-export const selectMovieCount = (state, id) => selectTickets(state).cart[id] || 0;
-
-export const selectTotal = (state) => selectTickets(state).total;
-
-export const selectCart = (state) => selectTickets(state).cart || {};
